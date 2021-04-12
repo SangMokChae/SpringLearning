0147a50c614fe3f26c31198955c06239a75903cf
sangmokchae <72491390+SangMokChae@users.noreply.github.com>
2021-04-12 22:51:14 +0900

    basic of spring
    basic of spring

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // spring은 @Controller라고 명시를 해주어야 한다.
 @Controller
@@ -12,5 +14,38 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = true) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http의 body에 직접 넣겠다.라는 것
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // hello spring
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    // json 방식으로 반환
+
+    static class Hello { // static 형식으로 만들면 class안에서  class형식으로 사용가능 ex) helloController.Hello
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
